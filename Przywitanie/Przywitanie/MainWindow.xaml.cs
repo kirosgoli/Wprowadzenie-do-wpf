1b46222389971d818ff002f568eccf30ed3cd3f0
DESKTOP-E6EFMF2\Karol Walczyna <kirosgoli@outlook.com>
2017-04-07 23:45:09 +0200

    Inicjalizujący Rozdział 1 i Rozdział 2 do zadań

---
+++ Przywitanie/Przywitanie/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Przywitanie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Witaj, świecie!");
+        }
+
+        private void btnTime_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DateTime data = DateTime.Now;
+            btnTime.Content = data.ToString("T");
+        }
+
+        private void btnTime_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btnTime.Content = "Czas";
+        }
+    }
+}
